640c022887e5c7a7ad6cdd2963e59c3754681b80
murilonicemento <murilo_nascimento22@outlook.com>
2022-12-07 17:19:16 -0300

    Inclusão de validação do formulário

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,6 +1,6 @@
 const button = document.querySelector("button");
 
-button.addEventListener("click", () => {
+button.addEventListener("click", (event) => {
     const email = document.getElementById("email").value;
     const password = document.getElementById("password").value;
     
@@ -8,6 +8,9 @@ button.addEventListener("click", () => {
         alert("Existem campos vazios no formulário. Preencha os campos vazios para validar o formulário");
     } else {
         alert("Formulário validado com sucesso");
+        console.log(email)
+        console.log(password)
+        event.preventDefault()
         email = "";
         password = "";
     }
